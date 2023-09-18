136ab92dd3274cf1dd93d8c00ca251d14c37116c
bimaxputra <bimaputrasejati9999@gmail.com>
2023-09-18 12:43:30 +0700

    Fourth Commit : edit config

--- phase3/ms-paylater/config/db.go
+++ phase3/ms-paylater/config/db.go
@@ -2,13 +2,14 @@ package config
 
 import (
 	"ms-paylater/model"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-	dsn := "host=localhost user=postgres password=postgres dbname=ms_paylater_p3 port=5432 sslmode=disable"
+	dsn := os.Getenv("DSN")
   	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
